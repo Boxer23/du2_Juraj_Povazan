9043157cc2a45c3c26266e8b4d0f0bee8809e785
Boxer23 <33222922+Boxer23@users.noreply.github.com>
2018-12-14 00:39:23 +0100

    Update du2.py

--- du2.py
+++ du2.py
@@ -1,17 +1,21 @@
 import json
 
-with open ("adresnebody.geojson", encoding ="utf-8") as f:
-    budovy = json.load(f)
+def read_json_file(nazov_suboru):
+    with open (nazov_suboru, encoding ="utf-8") as f:
+        return json.load(f)
+
+subor = input("Skopiruj nazov suboru: ")
+budovy = read_json_file(subor)
 
 features = budovy["features"]
-suradnice = []
-for x_y in features:
-    props = x_y["geometry"]
-    suradnice.append(props["coordinates"])
-    print(props["coordinates"])
 
-#for s in suradnice:
-    #print(s)
+def suradnice(features):
+    for x_y in features:
+        props = x_y["geometry"]
+        suradnice = props["coordinates"]
+        x = suradnice[0]
+        y = suradnice[1]
+        print(x, y)
 
-with open('data.txt', 'w') as outfile:
-    json.dump(suradnice, outfile)
+poloha = suradnice(features)
+
