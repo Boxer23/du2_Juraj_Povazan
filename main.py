4c0ed0d38a77119d8c1cd53ceadef0f218412ccf
Boxer23 <33222922+Boxer23@users.noreply.github.com>
2018-12-15 22:21:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import vypocty
-import json
 
 data = vypocty.read_json_file("adresnebody.geojson")
 features = data["features"]
 
+cluster_features = vypocty.default_cluster_id(features) #zavola funkciu default_cluster_id(features)
+
 obdlznik = vypocty.calcbox(features)
-print(obdlznik)
 
 new_features = vypocty.delenie(features, obdlznik)
 print(new_features)
 
-vypocet = vypocty.output(new_features, obdlznik)
-
-with open('data.txt', 'w') as outfile:
-    json.dump(vypocet, outfile)
+nazov_suboru = "output.geojson"
+vypocty.output(new_features, nazov_suboru)
