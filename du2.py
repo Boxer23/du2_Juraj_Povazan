b5a21e6d4852b01bf523c7ac4f9441abe1cbdd4c
Boxer23 <33222922+Boxer23@users.noreply.github.com>
2018-12-15 22:20:49 +0100

    Update du2.py

--- du2.py
+++ du2.py
@@ -5,6 +5,11 @@ def read_json_file(nazov_suboru):
     with open (nazov_suboru, encoding ="utf-8") as f:
         return json.load(f)
 
+def default_cluster_id(features):
+    for bod in features:
+        bod["cluster_id"] = "0"
+    return features
+
 def calcbox(features):
     body_x = []
     body_y = []
@@ -46,23 +51,32 @@ def delenie(features, obdlznik):
         pd_obdlznik = Rectangle(stred_AB, stred_BC)
         ph_obdlznik = Rectangle(S, C)
         lh_obdlznik = Rectangle(stred_DA, stred_CD)
-        #obdlznicky = [ld_obdlznik, pd_obdlznik, ph_obdlznik, lh_obdlznik]
-        def Cluster_ID(features, ld_obdlznik, pd_obdlznik, ph_obdlznik, lh_obdlznik):
-            if features == ld_obdlznik:
-                for id in features:
-                    id["Cluster_ID"] = "1"
-            if features == pd_obdlznik:
-                for id in features:
-                    id["Cluster_ID"] = "2"
-            if features == ph_obdlznik:
-                for id in features:
-                    id["Cluster_ID"] = "3"
-            if features == lh_obdlznik:
-                for id in features:
-                    id["Cluster_ID"] = "4"
-            return id
+        ld_body = zhluky(features,ld_obdlznik,"1")
+        pd_body = zhluky(features,pd_obdlznik,"2")
+        ph_body = zhluky(features, ph_obdlznik,"3")
+        lh_body = zhluky(features, lh_obdlznik,"4")
+        nove_ld_body = delenie(ld_body, ld_obdlznik)
+        nove_pd_body = delenie(pd_body, pd_obdlznik)
+        nove_ph_body = delenie(ph_body, ph_obdlznik)
+        nove_lh_body = delenie(lh_body, lh_obdlznik)
+        return nove_ld_body + nove_pd_body + nove_ph_body + nove_lh_body
     else:
-        exit()
-    return features
+        return features
 
-output = delenie
+def zhluky(features, obdlznik, cluster_id):
+    umiestnenie = []
+    for bod in features:
+        props = bod["geometry"]
+        poloha = props["coordinates"]
+        x = poloha[0]
+        y = poloha[1]
+        A = obdlznik.pts[0]
+        C = obdlznik.pts[2]
+        if x > A.x and x < C.x and y > A.y and y < C.y:
+            bod["cluster_id"] = bod["cluster_id"] + cluster_id
+            umiestnenie.append(bod)
+    return umiestnenie
+
+def output(features, nazov_suboru):
+    with open(nazov_suboru, "w") as outfile:
+        json.dump(features, outfile)
